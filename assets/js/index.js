4d433c5c4e489733903a05f1be794bc9664af72e
marynafedotova <mfedotova151@gmail.com>
2025-12-30 17:37:47 +0200

    added slider new products? fix btn all prodocts

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,61 +1,95 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const container = document.getElementById('news-product');
-    const btnShowAll = document.getElementById('show-all-btn');
+    const track = document.getElementById('news-product');
+    const prevBtn = document.getElementById('prevSlide');
+    const nextBtn = document.getElementById('nextSlide');
 
-    let allProducts = [];
+    let products = [];
+    let currentIndex = 0;
 
-fetch('assets/data/data.json')
-  .then(response => response.json())
-  .then(products => {
-      let desserts = products.filter(item => item["Категория"] === "Дессерт");
+    fetch('assets/data/data.json')
+        .then(res => res.json())
+        .then(data => {
+            products = data
+                .filter(item => item['Категория'] === 'Дессерт')
+                .sort((a, b) => b.ID - a.ID);
 
-      desserts.sort((a, b) => b.ID - a.ID);
-
-      allProducts = desserts;
-      renderProducts(desserts.slice(0, 4));
-
-      btnShowAll.addEventListener('click', () => {
-          container.innerHTML = "";
-          renderProducts(allProducts);
-          btnShowAll.style.display = "none";
-      });
-  })
-  .catch(error => console.error('Помилка завантаження файлу:', error));
-
+            renderProducts(products);
+            updateSlider();
+        })
+        .catch(err => console.error('JSON error:', err));
 
     function renderProducts(list) {
+        track.innerHTML = '';
+
         list.forEach(item => {
+            const kbju = item['КБЖУ'];
+
             const li = document.createElement('li');
             li.className = 'product-card';
 
             li.innerHTML = `
-                <a href="assets/pages/product.html?id=${item['ID']}" class="product-link">
-                    <div class="product-img">
-                        <img src="${item['Зображення']}" alt="${item['Назва']}" class="product-image">
+                <a href="assets/pages/product.html?id=${item.ID}" class="product-link">
+
+                    <div class="slide-img">
+                        <img src="${item['Зображення']}" alt="${item['Назва']}">
                     </div>
 
                     <div class="product-info">
                         <h3 class="product-title">${item['Назва']}</h3>
-                        <p class="product-weight">${item["Вага"]}</p>
+                        
 
-                        <div class="product-cart">
+                        <p class="product-description">${item['Опис']}</p>
+                        <div class="product-ingred-block">
+                            <div class="product-ingred-title">Склад:</div> 
+                            <div class="product-ingred">${item['Склад']}</div>
+                         </div>
+                        
+                        <div class="news-block-kbju">
+                        <h4 class="news-title-kbju">КБЖУ (100г):</h4>
+                        <ul class="product-kbju">
+                            <li>Калорії: ${kbju.Калорії} ккал</li>
+                            <li>Білки: ${kbju.Білки} г</li>
+                            <li>Жири: ${kbju.Жири} г</li>
+                            <li>Вуглеводи: ${kbju.Вуглеводи} г</li>
+                            <li>Хлібні одиниці: : ${kbju['Хлібні одиниці']}</li>
+                        </ul>
+                        </div>
+                        <div class="product-cart-slider">
+                        <div class="product-weight-slider">${item['Вага']}</div>
                             <div class="product-price">
                                 ${item['Ціна']}
-                                <div class="product-price-uah">грн</div>
+                                <span class="product-price-uah">грн</span>
                             </div>
-                    <button class="add-to-cart" data-id="${item['ID']}">
-                    <img src="assets/img/+.svg" alt="add to cart">
-                </button>
 
+                            <button class="add-to-cart" data-id="${item.ID}">
+                                <img src="assets/img/+.svg" alt="add to cart">
+                            </button>
                         </div>
                     </div>
-                </a>
 
+                </a>
             `;
 
-            container.appendChild(li);
+            track.appendChild(li);
         });
     }
+
+    function updateSlider() {
+        track.style.transform = `translateX(-${currentIndex * 100}%)`;
+    }
+
+    nextBtn.addEventListener('click', () => {
+        if (!products.length) return;
+        currentIndex = (currentIndex + 1) % products.length;
+        updateSlider();
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (!products.length) return;
+        currentIndex =
+            (currentIndex - 1 + products.length) % products.length;
+        updateSlider();
+    });
 });
 
 
@@ -108,9 +142,9 @@ function renderFavProducts(list) {
                             ${item['Ціна']}
                             <div class="product-price-uah">грн</div>
                         </div>
-                <button class="add-to-cart" data-id="${item['ID']}">
-                <img src="assets/img/+.svg" alt="add to cart">
-            </button>
+                            <button class="add-to-cart" data-id="${item['ID']}">
+                            <img src="assets/img/+.svg" alt="add to cart">
+                        </button>
 
                     </div>
                 </div>
