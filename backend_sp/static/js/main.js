5834f7b0c2a6a43a784e022e3eb8cdd08bbe16ec
marynafedotova <mfedotova151@gmail.com>
2026-01-08 14:01:28 +0200

    fix cart+language btn

--- backend_sp/static/js/main.js
+++ backend_sp/static/js/main.js
@@ -30,7 +30,7 @@ document.addEventListener('DOMContentLoaded', function() {
       const element = document.querySelector(hash);
       if (element) {
         element.scrollIntoView({ behavior: 'smooth' });
-        observer.disconnect(); // Зупинити спостереження після знаходження
+        observer.disconnect();
       }
     });
     
@@ -44,52 +44,16 @@ document.addEventListener('DOMContentLoaded', function() {
     setTimeout(() => observer.disconnect(), 5000);
   }
 });
-function getCart() {
-    let cart = sessionStorage.getItem('cart');
-    return cart ? JSON.parse(cart) : [];
-}
-
-
-function addToCart(productId) {
-
-    let cart = sessionStorage.getItem('cart');
-    cart = cart ? JSON.parse(cart) : [];
-
-    const existingItem = cart.find(item => item.id === productId);
-
-    if (existingItem) {
-        existingItem.qty += 1;
-    } else {
-        cart.push({ id: productId, qty: 1 });
-    }
-
-    sessionStorage.setItem('cart', JSON.stringify(cart));
-
-    openModal(); // ← ВАЖНО: вызываем модалку
-}
 
 // --- Модальное окно ---
+
 const modalOverlay = document.getElementById('modalOverlay');
-const closeModal = document.getElementById('closeModal');
-const continueShopping = document.getElementById('continueShopping');
-const goToCart = document.getElementById('goToCart');
 
-function openModal() {
+function showAddedMessage() {
   modalOverlay.classList.add('active');
+
+  setTimeout(() => {
+    modalOverlay.classList.remove('active');
+  }, 1500); // 1.5 секунды — оптимально
 }
 
-function hideModal() {
-  modalOverlay.classList.remove('active');
-}
-
-closeModal.addEventListener('click', hideModal);
-continueShopping.addEventListener('click', hideModal);
-
-// goToCart.addEventListener('click', () => {
-//     window.location.href = '/zero-caloriy/assets/pages/cart.html';
-// });
-
-
-modalOverlay.addEventListener('click', (e) => {
-  if (e.target === modalOverlay) hideModal();
-});
